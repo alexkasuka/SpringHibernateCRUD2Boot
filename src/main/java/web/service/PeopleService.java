1b7d828ff13a642827428e37e5d57628c325873b
Alexey Zarubin <alzarubin@gmail.com>
2022-09-22 10:10:49 +0300

    update from 2.3.1 task add PeopleService interface and its implementation

--- src/main/java/web/service/PeopleService.java
+++ src/main/java/web/service/PeopleService.java
@@ -1,46 +1,25 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import web.model.Person;
-import web.repository.PeopleRepository;
+
 
 import java.util.List;
-import java.util.Optional;
-
-@Service
-@Transactional(readOnly = true)
-public class PeopleService {
-
-    private final PeopleRepository peopleRepository;
-
-    @Autowired
-    public PeopleService(PeopleRepository peopleRepository) {
-        this.peopleRepository = peopleRepository;
-    }
 
 
-    public List<Person> findAll() {
-        return peopleRepository.findAll();
-    }
 
-    public Person find(int id) {
-        Optional<Person> foundPerson = peopleRepository.findById(id);
-        return foundPerson.orElse(null);
-    }
-    @Transactional
-    public void save(Person person) {
+public interface PeopleService {
 
-        peopleRepository.save(person);
-    }
-    @Transactional
-    public void update(int id, Person person) {
-        person.setId(id);
-        peopleRepository.save(person);
-    }
-    @Transactional
-    public void delete(int id) {
-        peopleRepository.deleteById(id);
-    }
+    List<Person> findAll();
+
+    Person find(int id);
+
+    void save(Person person);
+
+    void update(int id, Person person);
+
+    void delete(int id);
+
+
+
+
 }
---
+++ src/main/java/web/service/PeopleServiceImpl.java
@@ -0,0 +1,47 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.Person;
+import web.repository.PeopleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class PeopleServiceImpl implements PeopleService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleServiceImpl(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    public List<Person> findAll() {
+        return peopleRepository.findAll();
+    }
+
+    public Person find(int id) {
+        Optional<Person> foundPerson = peopleRepository.findById(id);
+        return foundPerson.orElse(null);
+    }
+    @Transactional
+    public void save(Person person) {
+
+        peopleRepository.save(person);
+    }
+    @Transactional
+    public void update(int id, Person person) {
+        person.setId(id);
+        peopleRepository.save(person);
+    }
+    @Transactional
+    public void delete(int id) {
+        peopleRepository.deleteById(id);
+    }
+
+}
+
